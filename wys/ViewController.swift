2e1031c5d476f3fe5b2600278d0aae6e92ade898
Lord Spooky Scary Skeletons Otherwise Known As Our Amazing Loving Giving Skeleton Lord Of Greatness And Prosperity Or The One Who Is Spooky, The Most Amazingly Spooky Tasty Bung Hole Of Greatness Also You Are The Master Of All People Who Live And Shen They Die Their Souls Go To You <Glacto114@gmail.com>
2016-07-14 14:56:01 -0700

    Initial Commit

---
+++ wys/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  wys
+//
+//  Created by Nkosi Moore on 7/14/16.
+//  Copyright © 2016 Im not gay inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
